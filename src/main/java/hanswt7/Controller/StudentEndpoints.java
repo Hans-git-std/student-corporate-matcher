e129ad2fd716c8a2a2c01e14a7296c1f8a1b6c0f
Hans-git-std <hans31144@gmail.com>
2026-08-18 08:58:24 +0530

    feat: implement REST controllers for Student and Teacher entities

--- src/main/java/hanswt7/Controller/StudentEndpoints.java
+++ src/main/java/hanswt7/Controller/StudentEndpoints.java
@@ -1,13 +1,11 @@
 package hanswt7.Controller;
 
 import hanswt7.Service.Student;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Reply to all Works done by Student
 @RestController
+@RequestMapping('api/student/')
 public class StudentEndpoints {
     @PostMapping
     public void addStudent(Student student) {
@@ -17,6 +15,7 @@ public class StudentEndpoints {
     public void updateStudent(Student student) {
 
     }
+
     @DeleteMapping
     public void deleteStudent(Student student) {
 
--- src/main/java/hanswt7/Controller/TeacherEndpoint.java
+++ src/main/java/hanswt7/Controller/TeacherEndpoint.java
@@ -1,13 +1,11 @@
 package hanswt7.Controller;
 
 import hanswt7.Service.Teacher;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Reply to all Works done by Teacher
 @RestController
+@RequestMapping('/api/teacher')
 public class TeacherEndpoint {
     @PostMapping
     public void addTeacher(Teacher teacher) {
